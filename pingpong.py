35d2f4e08f6d71f6cb9d62f042c10a8cb42970fc
Sunnatilla08 <107806873+Sunnatilla08@users.noreply.github.com>
2022-06-26 18:50:45 +0300

    Add files via upload
    version 1 oyun taslagi

---
+++ pingpong.py
@@ -0,0 +1,22 @@
+from pygame import*
+
+# oyun penceresinin oluşturulması
+pencere = display.set_mode((600,500))
+#pencere arka plan rengi RGB kod
+pencere.fill((200,255,255))
+
+
+game = True
+FPS = 60
+clock = time.Clock()
+
+# Oyun döngüsü
+while game:
+    # Eğer çarpı işaretine basılırsa pencereyi kapa
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    # ekranı güncelle
+    display.update()
+    clock.tick(FPS)
